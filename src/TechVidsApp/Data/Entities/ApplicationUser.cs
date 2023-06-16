84a3120c405278061c55922f75b36302166cebd4
chaitanya4294 <chaitanya.4294@gmail.com>
2023-06-16 13:21:53 +0530

    Apply first migration

---
+++ src/TechVidsApp/Data/Entities/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TechVidsApp.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+        public string AddressLine1 { get; set; }
+        
+        public string AddressLine2 { get; set;}
+
+        public string PostCode { get; set;}
+    }
+}
